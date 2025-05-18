187e911586fab700efb8cf29246712aff05d0279
lisleepsalot <lhuang02@risd.edu>
2025-05-17 23:36:36 -0400

    echoIpsum + iterator 1 done

--- js/data_tests.js
+++ js/data_tests.js
@@ -3,14 +3,14 @@ const dataTests = [
     index: 1,
     image: 'assets/test_cover1.webp',
     title: 'Echo Ipsum',
-    identifier: 'EchoIpsum',
+    identifier: 'echoipsum',
     description: `What begins as filler becomes message, then filler again—a cycle of becoming and unbecoming.`
   },
   {
     index: 2, // or whatever your second item’s index is
     image: 'assets/test_cover1.webp',
-    title: 'Test2',
-    identifier: 'test2',
+    title: 'Echo Ipsum(Iterator)',
+    identifier: 'Iterator',
     description: `Your second item’s description here…`
   },
   {
